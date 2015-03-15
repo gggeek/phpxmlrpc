14800d0367e92816271d87ee4202f1db915b17bc
gggeek <giunta.gaetano@gmail.com>
2015-03-15 23:47:28 +0000

    Yet one more try at travis+fcgi

---
+++ .travis/install_apache.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# enable php-fpm
+# @see https://github.com/travis-ci/travis-ci.github.com/blob/master/docs/user/languages/php.md#apache--php
+
+sudo apt-get install apache2 libapache2-mod-fastcgi
+sudo a2enmod rewrite actions fastcgi alias
+
+# enable php-fpm
+sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
+
+# configure apache virtual hosts
+sudo cp -f .travis/apache_vhost /etc/apache2/sites-available/default
+sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
+sudo service apache2 restart
---
+++ .travis/install_apache_hhvm.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# enable hhvm-fcgi
+# @see https://github.com/travis-ci/travis-ci.github.com/blob/master/docs/user/languages/php.md#apache--php
+
+sudo apt-get install apache2 libapache2-mod-fastcgi
+sudo a2enmod rewrite actions fastcgi alias
+
+# start HHVM
+hhvm -m daemon -vServer.Type=fastcgi -vServer.Port=9000 -vServer.FixPathInfo=true
+
+# configure apache virtual hosts
+sudo cp -f .travis/apache_vhost_hhvm /etc/apache2/sites-available/default
+sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
+sudo service apache2 restart
