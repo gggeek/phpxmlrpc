048358caae836a0e0d6a28956acf80c84e480aa3
gggeek <giunta.gaetano@gmail.com>
2015-03-20 23:31:55 +0000

    Rename the test files to insure a better test execution order

--- tests/4LocalhostMultiTest.php
+++ tests/4LocalhostMultiTest.php
@@ -5,7 +5,7 @@ include_once __DIR__ . '/../lib/xmlrpc_wrappers.inc';
 
 include_once __DIR__ . '/parse_args.php';
 
-include_once __DIR__ . '/LocalhostTest.php';
+include_once __DIR__ . '/3LocalhostTest.php';
 
 class LocalhostMultiTest extends LocalhostTest
 {
