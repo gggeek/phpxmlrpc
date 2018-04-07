7c6ae59a083bbb06be85d5ad08cacf9a7ca41c8a
gggeek <giunta.gaetano@gmail.com>
2018-04-07 01:15:16 +0100

    more php 7.2 test fixes

--- test/ci/travis/setup_php_fpm.sh
+++ test/ci/travis/setup_php_fpm.sh
@@ -9,7 +9,9 @@ fi
 if [ "$TRAVIS_PHP_VERSION" = "7.1" -a -n "$(ls -A ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d)" ]; then
   sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf
 fi
-
+if [ "$TRAVIS_PHP_VERSION" = "7.2" -a -n "$(ls -A ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d)" ]; then
+  sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf
+fi
 echo "cgi.fix_pathinfo = 1" > ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/zzz_testconfig.ini
 echo "always_populate_raw_post_data = -1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/zzz_testconfig.ini
 echo "include_path = '.:$(pwd)/lib'" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/zzz_testconfig.ini
