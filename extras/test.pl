cad2d502b4917eaa0deac3a46bd47b6e5d22021c
gggeek <giunta.gaetano@gmail.com>
2020-12-31 14:19:40 +0000

    modernize extras

--- extras/test.pl
+++ extras/test.pl
@@ -2,51 +2,60 @@
 
 use Frontier::Client;
 
-my $serverURL='http://phpxmlrpc.sourceforge.net/server.php';
+my $serverURL='http://gggeek.altervista.org/sw/xmlrpc/demo/server/server.php';
 
 # try the simplest example
 
-my $client = Frontier::Client->new( 'url' => $serverURL,
-		'debug' => 0, 'encoding' => 'iso-8859-1' );
+my $client = Frontier::Client->new(
+    'url' => $serverURL, 'debug' => 0, 'encoding' => 'iso-8859-1'
+);
 my $resp = $client->call("examples.getStateName", 32);
 
 print "Got '${resp}'\n";
 
 # now send a mail to nobody in particular
 
-$resp = $client->call("mail.send", ("edd", "Test",  
-	"Bonjour. Je m'appelle Gérard. Mañana. ", "freddy", "", "", 
-	'text/plain; charset="iso-8859-1"'));
+$resp = $client->call("mail.send", (
+    "edd",
+    "Test",
+    "Bonjour. Je m'appelle GÃ©rard. MaÃ±ana. ",
+    "freddy",
+    "",
+    "",
+    'text/plain; charset="iso-8859-1"')
+);
 
 if ($resp->value()) {
-	print "Mail sent OK.\n";
+    print "Mail sent OK.\n";
 } else {
-	print "Error sending mail.\n";
+    print "Error sending mail.\n";
 }
 
 # test echoing of characters works fine
 
-$resp = $client->call("examples.echo", 'Three "blind" mice - ' . 
-	"See 'how' they run");
+$resp = $client->call("examples.echo", 'Three "blind" mice - ' . "See 'how' they run");
 print $resp . "\n";
 
-# test name and age example. this exercises structs and arrays 
+# test name and age example. this exercises structs and arrays
 
-$resp = $client->call("examples.sortByAge", 
-											[ { 'name' => 'Dave', 'age' => 35},
-												{ 'name' => 'Edd', 'age' => 45 },
-												{ 'name' => 'Fred', 'age' => 23 },
-												{ 'name' => 'Barney', 'age' => 36 } ] );
+$resp = $client->call("examples.sortByAge",
+    [
+        { 'name' => 'Dave', 'age' => 35},
+        { 'name' => 'Edd', 'age' => 45 },
+        { 'name' => 'Fred', 'age' => 23 },
+        { 'name' => 'Barney', 'age' => 36 }
+    ]
+);
 
 my $e;
 foreach $e (@$resp) {
-	print $$e{'name'} . ", " . $$e{'age'} . "\n";
+    print $$e{'name'} . ", " . $$e{'age'} . "\n";
 }
 
 # test base64
 
-$resp = $client->call("examples.decode64", 
-											$client->base64("TWFyeSBoYWQgYSBsaXR0bGUgbGFtYiBTaGUgd" .
-																			"GllZCBpdCB0byBhIHB5bG9u"));
+$resp = $client->call("examples.decode64",
+    $client->base64("TWFyeSBoYWQgYSBsaXR0bGUgbGFtYiBTaGUgdGllZCBpdCB0byBhIHB5bG9u")
+);
 
 print $resp . "\n";
--- extras/test.py
+++ extras/test.py
@@ -1,37 +1,47 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python3
+# -*- coding: iso-8859-1 -*-
 
-from xmlrpclib import *
+import xmlrpc.client
+import base64
 import sys
 
-server = Server("http://phpxmlrpc.sourceforge.net/server.php")
+server = xmlrpc.client.ServerProxy("http://gggeek.altervista.org/sw/xmlrpc/demo/server/server.php")
 
 try:
-    print "Got '" + server.examples.getStateName(32) + "'"
+    print ("Got '" + server.examples.getStateName(32) + "'")
 
-    r = server.mail.send("edd", "Test",
-                         "Bonjour. Je m'appelle Gérard. Mañana. ", "freddy", "", "", 
-                         'text/plain; charset="iso-8859-1"')
-    if r:
-        print "Mail sent OK"
-    else:
-        print "Error sending mail"
-
+    # Disabled as demo servers often are prevented from sending mail...
+    #r = server.mail.send(
+    #    "edd", "Test",
+    #    "Bonjour. Je m'appelle Gérard. Mañana. ", "freddy", "", "", 
+    #    'text/plain; charset="iso-8859-1"'
+    #    )
+    #if r:
+    #    print ("Mail sent OK")
+    #else:
+    #    print ("Error sending mail")
 
     r = server.examples.echo('Three "blind" mice - ' + "See 'how' they run")
-    print r
+    print (r)
 
     # name/age example. this exercises structs and arrays
 
-    a = [ {'name': 'Dave', 'age': 35}, {'name': 'Edd', 'age': 45 },
-          {'name': 'Fred', 'age': 23}, {'name': 'Barney', 'age': 36 }]
+    a = [ 
+            {'name': 'Dave', 'age': 35}, {'name': 'Edd', 'age': 45 },
+            {'name': 'Fred', 'age': 23}, {'name': 'Barney', 'age': 36 }
+        ]
     r = server.examples.sortByAge(a)
-    print r
+    print (r)
 
     # test base 64
-    b = Binary("Mary had a little lamb She tied it to a pylon")
-    b.encode(sys.stdout)
+    b = 'Mary had a little lamb She tied it to a pylon'
+    b = base64.b64encode(b.encode('ascii'))
+    #print(b)
     r = server.examples.decode64(b)
-    print r
+    print (r)
     
-except Error, v:
-    print "XML-RPC Error:",v
+except xmlrpc.client.Fault as err:
+    print("A fault occurred")
+    print("Fault code: %d" % err.faultCode)
+    print("Fault string: %s" % err.faultString)
+
