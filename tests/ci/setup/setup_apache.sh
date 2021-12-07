239391c98a446647086cb650f95b13184242bfb4
gggeek <giunta.gaetano@gmail.com>
2021-12-07 12:03:19 +0000

    wip github actions CI

--- tests/ci/setup/setup_apache.sh
+++ tests/ci/setup/setup_apache.sh
@@ -23,6 +23,11 @@ rm /etc/apache2/mods-enabled/php* || true
 
 cp -f "$SCRIPT_DIR/../config/apache_vhost" /etc/apache2/sites-available/000-default.conf
 
+# default apache siteaccess found in GHA Ubuntu. We remove it just in case
+if [ -f /etc/apache2/sites-available/default-ssl.conf ]; then
+    rm /etc/apache2/sites-available/default-ssl.conf
+fi
+
 if [ -n "${TRAVIS}" -o -n "${GITHUB_ACTIONS}" ]; then
     echo "export TESTS_ROOT_DIR=$(pwd)" >> /etc/apache2/envvars
 else
