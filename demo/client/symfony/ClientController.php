2b15331c92651788cbc72638ae11408778a04293
gggeek <giunta.gaetano@gmail.com>
2023-01-31 20:05:44 +0000

    add to Sf demo the overtaking of the logger

--- demo/client/symfony/ClientController.php
+++ demo/client/symfony/ClientController.php
@@ -3,20 +3,28 @@
 namespace App\Controller;
 
 use PhpXmlRpc\Client;
+use PhpXmlRpc\PhpXmlRpc;
 use PhpXmlRpc\Request;
 use PhpXmlRpc\Value;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * An example usage of the xml-rpc client, configured as a service, from within a Symfony controller
+ */
 class ClientController extends AbstractController
 {
     protected $client;
 
-    public function __construct(Client $client)
+    public function __construct(Client $client, LoggerInterface $logger = null)
     {
         $this->client = $client;
+        if ($logger) {
+            PhpXmlRpc::setLogger($logger);
+        }
     }
 
     #[Route('/getStateName/{stateNo}', name: 'getstatename', methods: ['GET'])]
--- demo/server/symfony/ServerController.php
+++ demo/server/symfony/ServerController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use PhpXmlRpc\PhpXmlRpc;
 use PhpXmlRpc\Server;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,13 +13,16 @@ class ServerController extends AbstractController
 {
     protected $server;
 
-    public function __construct(Server $server)
+    public function __construct(Server $server, LoggerInterface $logger = null)
     {
         $this->server = $server;
+        if ($logger) {
+            PhpXmlRpc::setLogger($logger);
+        }
     }
 
     # This single method serves ALL the xml-rpc requests.
-    # The configuration for which xml-rpc methods exist and how they are handled is carried out in the Server service
+    # The configuration for which xml-rpc methods exist and how they are handled is carried out in the Server service definition
     #[Route('/xmlrpc', name: 'xml_rpc', methods: ['POST'])]
     public function serve(): Response
     {
