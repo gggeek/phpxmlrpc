b5f929568bec3d0b3705f4c76b234abaf644e893
gggeek <giunta.gaetano@gmail.com>
2025-02-18 18:22:05 +0000

    fix XXE test for php 5

--- tests/04ParsingTest.php
+++ tests/04ParsingTest.php
@@ -728,6 +728,10 @@ and there they were.</value></member><member><name>postid</name><value>7414222</
         $req = new \PhpXmlRpc\Request('hi');
         $resp = $req->parseResponse($xml, true);
         $val = $resp->value();
-        $this->assertequals('', $val->scalarVal());
+        if (version_compare(PHP_VERSION, '7.0.0', '>=')) {
+            $this->assertequals('', $val->scalarVal());
+        } else {
+            $this->assertequals('&xxe;', $val->scalarVal());
+        }
     }
 }
