190c378a7cb1a78f24295ed0a1f841d6950e3d96
gggeek <giunta.gaetano@gmail.com>
2025-10-07 15:02:06 +0000

    make it easier for subclasses to use a custom charset encoder

--- src/Traits/CharsetEncoderAware.php
+++ src/Traits/CharsetEncoderAware.php
@@ -7,11 +7,12 @@ use PhpXmlRpc\Helper\Charset;
 trait CharsetEncoderAware
 {
     protected static $charsetEncoder;
+    protected static $charsetEncoderClass = '\\PhpXmlRpc\\Helper\\Charset';
 
     public function getCharsetEncoder()
     {
         if (self::$charsetEncoder === null) {
-            self::$charsetEncoder = Charset::instance();
+            self::$charsetEncoder = call_user_func(array(static::$charsetEncoderClass, 'instance'));
         }
         return self::$charsetEncoder;
     }
